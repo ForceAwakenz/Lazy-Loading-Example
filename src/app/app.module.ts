d7e8a25c1e318a49769d0e89fe1e55d2bb94b978
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-13 08:48:33 +0300

    divided take out module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { TakeoutComponent } from './takeout/takeout.component';
+import { TakoutModule } from './takeout/takeout.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TakeoutComponent,
     MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TakoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/takeout/takeout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "../app-routing.module";
+import { TakeoutComponent } from "./takeout.component";
+
+
+@NgModule ({
+    declarations: [TakeoutComponent],
+    imports: [AppRoutingModule]
+})
+
+export class TakoutModule {}
