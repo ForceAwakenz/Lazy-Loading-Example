e5d0ee1dc19a639cc8deb18bb135d5325e955f35
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-13 08:34:22 +0300

    created routing for basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { TakeoutComponent } from './takeout/takeout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'takeout', component: TakeoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
