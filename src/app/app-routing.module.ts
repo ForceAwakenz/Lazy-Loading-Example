e7fcf48621b6454c3216391911857b9a41007934
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-14 17:08:23 +0300

    additional code from meetup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+// import { TakeoutComponent } from './takeout/takeout.component';
 
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'takeout', loadChildren: () => import('./takeout/takeout.module').then(m => m.TakeoutModule)},
+  // {path: 'takeout', component: TakeoutComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy:PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,6 +2,6 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  template: `<a routerLink="takeout">Link to Takeout Module</a>`,
+  template: `<a routerLink="takeout">Link to the Takeout Module</a>`,
 })
 export class MainComponent {}
