585a51b5cd4069bc2122673034be8f2a69101749
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-10-14 11:41:58 +0300

    pictures added

--- src/app/takeout/takeout.component.ts
+++ src/app/takeout/takeout.component.ts
@@ -2,6 +2,12 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-takeout',
-  template: `<p>Takeout Module!</p>`,
+  template: `<p>Takeout Module!</p>
+            <img src="../assets/1.jpg">
+            <img src="../assets/2.jpg">
+            <img src="../assets/3.jpg">
+            <img src="../assets/4.jpg">
+            <img src="../assets/5.jpg">
+  `,
 })
 export class TakeoutComponent {}
--- src/app/takeout/takeout.module.ts
+++ src/app/takeout/takeout.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "../app-routing.module";
+import { TakeoutRoutingModule } from "./takeout-routing.module";
 import { TakeoutComponent } from "./takeout.component";
 
 @NgModule ({
     declarations: [TakeoutComponent],
-    imports: [AppRoutingModule]
+    imports: [TakeoutRoutingModule]
 })
 
 export class TakeoutModule {}
